3a2f6e639af8f0943c6db0a21f284f1f8f3b4753
SamuelVarolloDev1 <varollosamuel@gmail.com>
2026-08-10 02:51:48 -0300

    Lumi para professores
    Ajustes

--- PROFESSOR/api/firebase.php
+++ PROFESSOR/api/firebase.php
@@ -2,53 +2,38 @@
 declare(strict_types=1);
 
 use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-function firebaseAuth(): Auth
+function firebaseAuth()
 {
     $projectId = getenv('FIREBASE_PROJECT_ID');
     $clientEmail = getenv('FIREBASE_CLIENT_EMAIL');
     $privateKey = getenv('FIREBASE_PRIVATE_KEY');
 
-    if (!$projectId) {
+    if (!$projectId || !$clientEmail || !$privateKey) {
         throw new RuntimeException(
-            'FIREBASE_PROJECT_ID não configurado.'
+            'Variáveis do Firebase não configuradas.'
         );
     }
 
-    if (!$clientEmail) {
-        throw new RuntimeException(
-            'FIREBASE_CLIENT_EMAIL não configurado.'
-        );
-    }
-
-    if (!$privateKey) {
-        throw new RuntimeException(
-            'FIREBASE_PRIVATE_KEY não configurado.'
-        );
-    }
-
-    $privateKey = str_replace(
-        ['\\n', '\\r\\n'],
-        ["\n", "\r\n"],
-        $privateKey
-    );
+    $privateKey = str_replace('\n', "\n", $privateKey);
 
     $serviceAccount = [
         'type' => 'service_account',
         'project_id' => $projectId,
+        'private_key_id' => '',
+        'private_key' => $privateKey,
         'client_email' => $clientEmail,
-        'private_key' => $privateKey
+        'client_id' => '',
+        'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+        'token_uri' => 'https://oauth2.googleapis.com/token',
+        'auth_provider_x509_cert_url' =>
+            'https://www.googleapis.com/oauth2/v1/certs',
+        'client_x509_cert_url' => ''
     ];
 
-    $factory = new Factory();
-
-    $factory = $factory->withServiceAccount(
-        $serviceAccount
-    );
-
-    return $factory->createAuth();
+    return (new Factory())
+        ->withServiceAccount($serviceAccount)
+        ->createAuth();
 }
-?>
