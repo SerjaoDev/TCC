c3c6aac5d1b2dc588f947395f5207a3ba6d034e3
SamuelVarolloDev1 <varollosamuel@gmail.com>
2026-08-11 01:04:48 -0300

    Lumi para professores
    Tentando fazer o Login com Google funcionar

--- PROFESSOR/api/firebase.php
+++ PROFESSOR/api/firebase.php
@@ -2,10 +2,11 @@
 declare(strict_types=1);
 
 use Kreait\Firebase\Factory;
+use Kreait\Firebase\Contract\Auth;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-function firebaseAuth()
+function firebaseAuth(): Auth
 {
     $projectId = getenv('FIREBASE_PROJECT_ID');
     $clientEmail = getenv('FIREBASE_CLIENT_EMAIL');
@@ -38,18 +39,13 @@ function firebaseAuth()
     $serviceAccount = [
         'type' => 'service_account',
         'project_id' => $projectId,
-        'private_key_id' => '',
-        'private_key' => $privateKey,
         'client_email' => $clientEmail,
-        'client_id' => '',
-        'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-        'token_uri' => 'https://oauth2.googleapis.com/token',
-        'auth_provider_x509_cert_url' =>
-            'https://www.googleapis.com/oauth2/v1/certs',
-        'client_x509_cert_url' => ''
+        'private_key' => $privateKey,
     ];
 
-    return (new Factory())
+    $factory = (new Factory)
         ->withServiceAccount($serviceAccount)
-        ->createAuth();
+        ->withProjectId($projectId);
+
+    return $factory->createAuth();
 }
