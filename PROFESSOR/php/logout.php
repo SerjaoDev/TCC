47e0cf6fcf11b97b2288a6db1ff0bfd66cabcfee
SamuelVarolloDev1 <varollosamuel@gmail.com>
2026-08-13 22:30:07 -0300

    Lumi para professores
    Ajustes no logout

--- PROFESSOR/php/logout.php
+++ PROFESSOR/php/logout.php
@@ -1,23 +1,26 @@
 <?php
+declare(strict_types=1);
+
 session_start();
 
 $_SESSION = [];
 
-if (ini_get("session.use_cookies")) {
+if (ini_get('session.use_cookies')) {
     $params = session_get_cookie_params();
+
     setcookie(
         session_name(),
-        "",
+        '',
         time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
     );
 }
 
 session_destroy();
 
-header("Location: ../index.html");
-exit();
+header('Location: index.php');
+exit;
 ?>
